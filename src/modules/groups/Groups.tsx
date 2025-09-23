9d98be7e8ced1adf689e250177f67286f94c749b
gen <kyliki@yandex.ru>
2025-09-23 22:01:43 +0300

    groups, sportsmen

--- src/modules/groups/Groups.tsx
+++ src/modules/groups/Groups.tsx
@@ -1,7 +1,22 @@
+import { Paper, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
 export default function Groups() {
   return (
     <div>
       <div>Groups</div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Фамилия</TableCell>
+              <TableCell>Имя</TableCell>
+              <TableCell>Отчество</TableCell>
+              <TableCell width={'1%'}></TableCell>
+              <TableCell width={'1%'}></TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- src/modules/sportsmen/Sportsmen.tsx
+++ src/modules/sportsmen/Sportsmen.tsx
@@ -1,7 +1,53 @@
+import { Checkbox, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import type { ChangeEvent } from 'react';
+const theadStyles = {
+  fontWeight: 'bold',
+};
 export default function Sportsmen() {
+  const fields = [];
+  const sporstmen = [];
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>, personId: number) => {
+    console.log(e, personId);
+  };
+
   return (
     <div>
       <div>sportsmen</div>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            {fields.map((field) => (
+              <TableCell
+                key={field.key as string}
+                sx={{
+                  ...theadStyles,
+                }}
+              >
+                {field.label}
+              </TableCell>
+            ))}
+            <TableCell sx={theadStyles} width={'1%'}>
+              Наблюдать
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sporstmen.map((entity) => (
+            <TableRow key={entity.id}>
+              {fields.map((field) => (
+                <TableCell>{entity[field.key] as string}</TableCell>
+              ))}
+              <TableCell align={'center'}>
+                <Checkbox
+                  key={`${entity.id}${entity.id}`}
+                  onChange={(e) => onChange(e, entity.id)}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
