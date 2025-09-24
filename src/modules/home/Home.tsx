daff1589fa10b3cc2f6688a5c8ecda078903d7fe
TimPulin <tigeronsun@gmail.com>
2025-09-24 16:43:17 +0300

    signed sportsmen list

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,7 +1,9 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import SportsmenListWithSignButton from '@/common/components/sportsmen/SportsmenListWithSignButton.tsx';
 import { useSportsmanApi } from '@/common/api/sportsman/hooks/use-sportsman-api.hook.ts';
 import { useModal } from '@/app/providers/global-modal/use-modal.hook.ts';
+import { Link } from 'react-router-dom';
+import BaseTile from '@/common/tile/BaseTile.tsx';
 
 export default function Home() {
   const { openModal } = useModal();
@@ -22,6 +24,31 @@ export default function Home() {
   };
   return (
     <Box>
+      {sportsmen && sportsmen.length > 0 && (
+        <Typography variant="h6"> Спортсмены, на которых вы подписаны</Typography>
+      )}
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          gap: 1,
+          flexWrap: 'wrap',
+        }}
+      >
+        {sportsmen &&
+          sportsmen.map((entity) => (
+            <Link key={entity.id} to={`/sportsman/${entity.id}`} style={{ textDecoration: 'none' }}>
+              <BaseTile>
+                <Box>
+                  <Typography variant="body2" color="primary.contrastText">
+                    {entity.lastName} {entity.firstName}
+                  </Typography>
+                </Box>
+              </BaseTile>
+            </Link>
+          ))}
+      </Box>
+
       <Button variant="contained" fullWidth sx={{ mt: 2 }} onClick={onButtonClick}>
         Подписаться на спортсмена
       </Button>
